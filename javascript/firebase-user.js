ddd0d254be8bf11d49538d7e4264170b2b1a4457
Chukwudi Wisdom <override-bot@users.noreply.github.com>
2021-04-24 18:57:49 +0200

    add courses page 2/2

--- javascript/firebase-user.js
+++ javascript/firebase-user.js
@@ -1,5 +1,7 @@
 var user;
 var db = firebase.firestore();
+var subArray = [];
+var getter = firebase.auth().currentUser;
 
 function registerUser() {
     var email = document.getElementById('email').value;
@@ -67,6 +69,8 @@ function displayUser() {
     studRef.get().then((doc) => {
         document.getElementById("studName").innerText = doc.data()['name'];
         document.getElementById("regn").innerText = doc.data()['regNumber'];
+        array = doc.data()['courses'];
+        array.forEach(show)
         console.log(doc.data());
     })
 }
@@ -80,21 +84,67 @@ function displayCourses() {
     var cRef = db.collection("courses").doc("LtoCEGLl5xCFsb405pAB");
     cRef.get().then((doc) => {
         var array = doc.data()['courses']
-        for (i = 0; i < array.length; i++) {
-            var larger = document.getElementById('contain');
-            var label = document.createElement('label');
-            label.className = 'label';
-            label.innerText = array[i];
-            var input = document.createElement('input');
-            input.type = 'checkbox';
-            input.className = 'input';
-            var check = document.createElement('span');
-            check.className = 'checkmark';
-            var br = document.createElement('br');
-            label.append(input);
-            label.append(check);
-            label.append(br);
-            larger.append(label);
-        }
+        array.forEach(create)
     })
+
+}
+
+function create(item) {
+    var larger = document.getElementById('contain');
+    var label = document.createElement('label');
+    label.className = 'label';
+    label.innerText = item;
+
+    var input = document.createElement('input');
+    input.type = 'checkbox';
+    input.className = 'input';
+    input.value = item
+    input.onchange = function() {
+        updateSubjects(item)
+    }
+    var check = document.createElement('span');
+    check.className = 'checkmark';
+    var br = document.createElement('br');
+    label.append(input);
+    label.append(check);
+    label.append(br);
+    larger.append(label);
+}
+
+function updateSubjects(str) {
+
+    if (!subArray.includes(str)) {
+        subArray.push(str)
+    } else {
+        subArray.pop(str)
+    }
+    console.log(subArray)
+}
+var studName;
+var studReg;
+
+function studSub() {
+    var use = firebase.auth().currentUser;
+    var studRef = db.collection("Students").doc(use.uid);
+    studRef.get().then((doc) => {
+        db.collection("Students").doc(use.uid).set({
+            name: doc.data()['name'],
+            regNumber: doc.data()['regNumber'],
+            courses: subArray
+        }).then(() => {
+            alert("upload successful")
+            window.location.href = "dashboard.html";
+        }).catch((error) => {
+            alert(error);
+        });
+    })
+
+}
+
+function show(item) {
+    var larger = document.getElementById('shhh');
+    var text = document.createElement("h3")
+    text.className = "subr";
+    text.innerText = item;
+    larger.append(text)
 }
