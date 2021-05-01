57bbf1a4bf954abf54dbb6a25fcf7a4e8ad4a95d
Johnpaul <polymenjohn1@gmail.com>
2021-05-01 08:50:48 +0100

    make it better

--- nuxt.config.js
+++ nuxt.config.js
@@ -70,8 +70,8 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-    publicPath: "_nuxt/dist",
-    buildDir :"_nuxt/dist"
+    // publicPath: "_nuxt/dist",
+    // buildDir :"_nuxt/dist"
   },
   router:{
     // base: '/023-CS-UNN.github.io/'
